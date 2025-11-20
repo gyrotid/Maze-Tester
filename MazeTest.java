d8b01ff55a1733a54563171cd012593ad3a12718
Dalton <daltonamurray@proton.me>
2025-11-20 19:32:16 +0000

    added maze.dat and initialied methods for getting the maze

--- MazeTest.java
+++ MazeTest.java
@@ -1,7 +1,14 @@
 public class MazeTest
 {
+    MazeTest maze = new MazeTest();
+    
+    public getMaze(maze)
+    {
+        //This function gets the data from maze.dat
+    }
+
     public static void main(String[] args)
     {
-        
+        //this function calls the maze object (MazeTest maze) that i decalared globally
     }
 }
