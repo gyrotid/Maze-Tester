5eb1d7be03c2e914ce081b6342388192288212d6
Dalton <daltonamurray@proton.me>
2025-11-23 02:47:36 +0000

    Adjusted names

--- Maze.java
+++ Maze.java
@@ -1,14 +1,23 @@
-public class MazeTest
+public class Maze
 {
-    MazeTest maze = new MazeTest();
+    public Maze()
+    {
+        //the constructor for the MazeTest class
+    }
     
     public getMaze(maze)
     {
         //This function gets the data from maze.dat
     }
 
+    public String toString()
+    {
+        System.out.println("Maze: ");
+    }
+
     public static void main(String[] args)
     {
         //this function calls the maze object (MazeTest maze) that i decalared globally
+        Maze newMaze = new Maze();
     }
 }
