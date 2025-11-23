d8c62705214c05dcf36ce0013c3c1dd39342e5c5
Dalton <daltonamurray@proton.me>
2025-11-23 17:58:23 +0000

    working on it...

--- Maze.java
+++ Maze.java
@@ -3,23 +3,26 @@ import java.io.*;
 
 public class Maze
 {
-    char aMaze[20][20]; 
+    char[][] aMaze = new char[20][20]; 
     boolean solveable = false;
     
     public Maze()
     {
         //the constructor for the Maze class
-        Path file = Paths.get("/workspaces/Maze-Tester/maze.dat");
-        InputSteam input = null;
         try
         {
-            input = Files.newInputStream(file);
-            BufferedReader reader = new
-                BufferedReader(new InputStreamReader(input));
+            BufferedReader reader = new BufferedReader(new FileReader("/workspaces/Maze-Tester/maze.dat"));
             String s = null;
-            while (s = reader.readLine() != null)
-                System.out.println(s);
-            input.close();
+            int currentRow = 0;
+            while ((s = reader.readLine()) != null)
+            {
+                for (int i = 0; i < aMaze.length; i++)
+                {
+                    
+                }
+                currentRow++;
+            }
+            reader.close();
         }
         catch (IOexception e)
         {
@@ -43,3 +46,11 @@ public class Maze
 
     }
 }
+
+/* 
+Need to treat each line of the file as a sequence of characters
+Each character in the that line belongs in one column of the same row
+I need to index the characters of the line directly
+Then fill the row and column into variables
+Afterwards move on to the next row
+*/
